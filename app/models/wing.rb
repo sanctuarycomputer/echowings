a9d2689b3b5daff9157045540e1f31725c5a8a55
Hugh Francis <me@hughfrancis.me>
2016-11-16 10:58:54 -0500

    Adds Submissions

--- app/models/wing.rb
+++ app/models/wing.rb
@@ -1,6 +1,10 @@
 class Wing < ApplicationRecord
+  validates :email, email: true
+  validates_presence_of :polarity
+
   enum polarity: {
     left: 0,
-    right: 1
+    right: 1,
+    middle: 2
   }
 end
--- client/app/bundles/Echowings/containers/Echowings.jsx
+++ client/app/bundles/Echowings/containers/Echowings.jsx
@@ -1,19 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import EchowingsWidget from '../components/EchowingsWidget';
 
+function submitWing(email, polarity) {
+  return fetch('/wings', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, polarity })
+  });
+}
+
 export default class HelloWorld extends Component {
   static propTypes = {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
   };
 
-  constructor(props, context) {
-    super(props, context);
-    this.state = { name: this.props.name };
-  }
-
   render() {
     return (
-      <EchowingsWidget name={this.state.name} />
+      <EchowingsWidget submitWing={submitWing} />
     );
   }
 }
